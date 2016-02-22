266745a165347be2a33c44d078826388d89c8d1f
Luicil Fernandes <luicilfernandes@hotmail.com>
2016-02-22 19:11:28 -0300

    Frases ok.

---
+++ Relogio/CustomTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CustomTableViewCell.swift
+//  Relogio
+//
+//  Created by Luicil Fernandes on 21/02/16.
+//  Copyright © 2016 Luicil Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var CustomLabel: UILabel!
+    @IBOutlet weak var excBtn: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Relogio/FrasesViewController.swift
+++ Relogio/FrasesViewController.swift
@@ -8,12 +8,24 @@
 
 import UIKit
 
-class FrasesViewController: UIViewController {
+class FrasesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var txtField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tableData : [String] = []
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        txtField.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+        checkForSavedData()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +33,6 @@ class FrasesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
     /*
     // MARK: - Navigation
 
@@ -31,5 +42,62 @@ class FrasesViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CustomTableViewCell", forIndexPath: indexPath) as! CustomTableViewCell
+        cell.CustomLabel.text = tableData[indexPath.row]
+        cell.excBtn.tag = indexPath.row
+        cell.excBtn.addTarget(self, action: "excBtn:", forControlEvents: .TouchUpInside)
+        return cell
+    }
+    
+    func saveData() {
+        defaults.setObject(tableData as [NSString], forKey: "frases")
+        defaults.synchronize()
+    }
+    
+    func checkForSavedData() ->Bool {
+        if let savedData : [NSString] = defaults.objectForKey("frases") as? [NSString] {
+            for data in savedData {
+                tableData.append(data as String)
+            }
+            tableData.sortInPlace()
+            tableView.reloadData()
+            return true
+        }
+        return false
+    }
+    
+    @IBAction func btnIncuir(sender: AnyObject) {
+        if txtField.text != "" {
+            tableData.append(txtField.text!)
+            tableData.sortInPlace()
+            tableView.reloadData()
+            saveData()
+            txtField.text = ""
+        }
+    }
+    
+    @IBAction func excBtn(sender: UIButton) {
+        let pos : Int = sender.tag
+        tableData.removeAtIndex(pos)
+        saveData()
+        tableView.reloadData()
+    }
 }
+
+extension FrasesViewController : UITextFieldDelegate {
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, withEvent: event)
+    }
+}
+
