c395ac1c56c65e00e318570248e41d9cac8ea543
Luicil Fernandes <luicilfernandes@hotmail.com>
2016-03-11 07:59:24 -0300

    Tela de cores. Em andamento.

--- Relogio/ChangeColorViewController.swift
+++ Relogio/ChangeColorViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class ChangeColorViewController: UIViewController {
 
+    
+    @IBOutlet weak var sliderVermelho: UISlider!
+    @IBOutlet weak var sliderAzul: UISlider!
+    @IBOutlet weak var sliderVerde: UISlider!
+    @IBOutlet weak var imageViewCor: UIImageView!
+    
+    var titulo : String?
+    var chave : String?
+    var cor : UIColor?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        imageViewCor.layer.borderWidth = 1.0
+        var red : CGFloat = 0.0
+        var green : CGFloat = 0.0
+        var blue : CGFloat = 0.0
+        var alpha : CGFloat = 1.0
+        cor?.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,5 +49,35 @@ class ChangeColorViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    func ChangeColor() {
+        let r : CGFloat = CGFloat(self.sliderVermelho.value) / CGFloat(255.0)
+        let g : CGFloat = CGFloat(self.sliderVerde.value) / CGFloat(255.0)
+        let b : CGFloat = CGFloat(self.sliderAzul.value) / CGFloat(255.0)
+        
+        self.imageViewCor.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1.0)
+        
+    }
+    
+    
+    @IBAction func sliderVermelhoAct(sender: UISlider) {
+        self.ChangeColor()
+    }
+    
+    @IBAction func sliderAzulAct(sender: UISlider) {
+        self.ChangeColor()
+    }
+    
+    @IBAction func sliderVerde(sender: UISlider) {
+        self.ChangeColor()
+    }
+    
+    @IBAction func cancelarBtn(sender: UIButton) {
+    }
+    
+    @IBAction func confirmarBtn(sender: UIButton) {
+    }
+    
 
 }
